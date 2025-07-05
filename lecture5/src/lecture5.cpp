7b581176a4753d34a1c08c3c89abb64e9ec5e795
Zeid Kootbally <zeidk@umd.edu>
2025-07-05 01:59:10 -0400

    Updated source code for Lecture 5

--- lecture5/src/lecture5.cpp
+++ lecture5/src/lecture5.cpp
@@ -2,7 +2,7 @@
  * @file lecture5.cpp
  * @author Zeid Kootbally (zeidk@umd.edu)
  * @brief Code snippets for lecture 5 on functions
- * @version 0.1
+ * @version 1.0
  * @date 2025-07-03
  *
  * @copyright Copyright (c) 2025
@@ -13,24 +13,51 @@
 #include <iomanip>
 #include <iostream>
 #include <string>
-#include <typeinfo>  // needed for typeid
+#include <typeinfo> // needed for typeid
 #include <vector>
 
 // function declaration/prototype
 int add_numbers(int a, int b);
 
 // function definition/implementation
-int add_numbers(int a, int b) {
+int add_numbers(int a, int b)
+{
     return a + b;
 }
 
-int main() {
-    // //==============
-    // //======== 1
-    // //==============
-
-    // //==============
-    // //======== 2
-    // //==============
-    std::cout << add_numbers(3, 5) << '\n';
+std::vector<int> create_vector()
+{
+    return std::vector<int>{1, 2, 3};
+    //     ^^^^^^^^^^^^^^^^^^^^^^^^^
+    //     Prvalue - guaranteed RVO
 }
+
+
+
+int main()
+{
+    //==============
+    //======== 1
+    //==============
+
+    //==============
+    //======== 2
+    //==============
+
+    // std::cout << add_numbers(3, 5) << '\n';
+
+    //==============
+    //======== 11
+    //==============
+
+    // std::vector<int> vect{create_vector()};
+    // // Construction directly in vect's location
+    // for (const auto &item: vect){
+    //     std::cout << item << '\n';
+    // }
+
+    //==============
+    //======== 12
+    //==============
+}
+
