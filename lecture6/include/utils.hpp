ceaaed4848bac437cd595cd4d0f17315b708ca94
Zeid Kootbally <zeidk@umd.edu>
2025-07-06 19:51:49 -0400

    Fixed formatting

--- lecture6/include/utils.hpp
+++ lecture6/include/utils.hpp
@@ -1,9 +1,9 @@
-#include<memory>
+#include <memory>
 
-void legacy_function(int* ptr);
+void legacy_function(int *ptr);
 void display(std::unique_ptr<int> v);
-void reseat(std::unique_ptr<int>& v);
+void reseat(std::unique_ptr<int> &v);
 std::unique_ptr<int> create_resource();
 void sink_shared_ptr(std::shared_ptr<int> ptr);
-void reseat_shared_ptr(std::shared_ptr<int>& ptr);
+void reseat_shared_ptr(std::shared_ptr<int> &ptr);
 std::shared_ptr<int> create_shared_resource();
