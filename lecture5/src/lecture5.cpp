462186dd448dfe7ada1967ba02cc62e7f4b4b19d
Zeid Kootbally <zeidk@umd.edu>
2025-07-06 19:53:49 -0400

    Fixed formatting in Lecture 5 source code

--- lecture5/src/lecture5.cpp
+++ lecture5/src/lecture5.cpp
@@ -17,208 +17,180 @@
 //======== 1
 // Exercise #1
 
-int main()
-{
-    //==============
-    //======== 1
-    //==============
+int main() {
+  //======== 1
+  //==============
 
-    //==============
-    //======== 2
-    //==============
-    // print_hello();
-    // std::cout << "exit main\n";
+  //======== 2
+  //==============
+  // print_hello();
+  // std::cout << "exit main\n";
 
-    //==============
-    //======== 3
-    //==============
-    // print_hello();
-    // std::cout << "exit main\n";
+  //======== 3
+  //==============
+  // print_hello();
+  // std::cout << "exit main\n";
 
-    //==============
-    //======== 4
-    //==============
-    // prompt_user();
+  //======== 4
+  //==============
+  // prompt_user();
 
-    //==============
-    //======== 5
-    //==============
-    // int a{5};
-    // add_ten(a);
-    // std::cout << a << '\n'; // 5
+  //======== 5
+  //==============
+  // int a{5};
+  // add_ten(a);
+  // std::cout << a << '\n'; // 5
 
-    //==============
-    //======== 6
-    //==============
-    // int a{5};
-    // add_ten(a);
-    // std::cout << a << '\n'; // 15
+  //======== 6
+  //==============
+  // int a{5};
+  // add_ten(a);
+  // std::cout << a << '\n'; // 15
 
-    //==============
-    //======== 7
-    //==============
-    // int x{5};
-    // int y{10};
+  //======== 7
+  //==============
+  // int x{5};
+  // int y{10};
 
-    // std::cout << x << ", " << y << '\n'; // 5, 10
-    // swap_values(x, y);
-    // std::cout << x << ", " << y << '\n'; // 10, 5
+  // std::cout << x << ", " << y << '\n'; // 5, 10
+  // swap_values(x, y);
+  // std::cout << x << ", " << y << '\n'; // 10, 5
 
-    //==============
-    //======== 8
-    //==============
-    // std::vector<int> num_vect{1, 2, 3, 4, 5};
-    // push_ten(num_vect);
-    // for (const auto &item : num_vect)
-    // {
-    //     std::cout << item << " "; // 1 2 3 4 5 6
-    // }
-    // std::cout << '\n';
+  //======== 8
+  //==============
+  // std::vector<int> num_vect{1, 2, 3, 4, 5};
+  // push_ten(num_vect);
+  // for (const auto &item : num_vect)
+  // {
+  //     std::cout << item << " "; // 1 2 3 4 5 6
+  // }
+  // std::cout << '\n';
 
-    //==============
-    //======== 9
-    //==============
-    // std::vector<int> num_vect{1, 2, 3, 4, 5};
-    // print_vector(num_vect);
+  //======== 9
+  //==============
+  // std::vector<int> num_vect{1, 2, 3, 4, 5};
+  // print_vector(num_vect);
 
-    //==============
-    //======== 10
-    //==============
-    // int a{5};
-    // add_ten(&a);
-    // std::cout << a << '\n'; // 15
+  //======== 10
+  //==============
+  // int a{5};
+  // add_ten(&a);
+  // std::cout << a << '\n'; // 15
 
-    //==============
-    //======== 11
-    //==============
+  //======== 11
+  //==============
 
-    // count_calls();  // 1 time(s);
-    // count_calls();  // 2 time(s);
-    // count_calls();  // 3 time(s);
+  // count_calls();  // 1 time(s);
+  // count_calls();  // 2 time(s);
+  // count_calls();  // 3 time(s);
 
-    //==============
-    //======== 12
-    //==============
+  //======== 12
+  //==============
 
-    // add_to_sum(1); // 1
-    // add_to_sum(2); // 3
-    // add_to_sum(3); // 6
-    // add_to_sum(4); // 10
+  // add_to_sum(1); // 1
+  // add_to_sum(2); // 3
+  // add_to_sum(3); // 6
+  // add_to_sum(4); // 10
 
-    //==============
-    //======== 13
-    //==============
-    // std::vector<int> vect{create_vector()};
-    // // Construction directly in vect's location
+  //======== 13
+  //==============
+  // std::vector<int> vect{create_vector()};
+  // // Construction directly in vect's location
 
-    //==============
-    //======== 14
-    //==============
-    // std::vector<int> result{create_vector()};
+  //======== 14
+  //==============
+  // std::vector<int> result{create_vector()};
 
-    //==============
-    //======== 15
-    //==============
+  //======== 15
+  //==============
 
-    //==============
-    //======== 16
-    //==============
-    // std::vector<int> v1{};
-    // v1 = create_vector();                // No copy elision
-    // std::cout << "&v1: " << &v1 << '\n'; //@2
+  //======== 16
+  //==============
+  // std::vector<int> v1{};
+  // v1 = create_vector();                // No copy elision
+  // std::cout << "&v1: " << &v1 << '\n'; //@2
 
-    //==============
-    //======== 17
-    //==============
-    // std::vector<int> my_vector{1, 2, 3, 4, 5};
+  //======== 17
+  //==============
+  // std::vector<int> my_vector{1, 2, 3, 4, 5};
 
-    // // Get a reference to the element at index 2
-    // int &ref = get_element(my_vector, 2);
+  // // Get a reference to the element at index 2
+  // int &ref = get_element(my_vector, 2);
 
-    // // Modify the element via the reference
-    // ref = 10;
+  // // Modify the element via the reference
+  // ref = 10;
 
-    // // Check the new value
-    // std::cout << my_vector[2] << '\n';
+  // // Check the new value
+  // std::cout << my_vector[2] << '\n';
 
-    //==============
-    //======== 18
-    //==============
-    // std::vector<int> my_vector = {1, 2, 3, 4, 5};
+  //======== 18
+  //==============
+  // std::vector<int> my_vector = {1, 2, 3, 4, 5};
 
-    // // Get a reference to the element at index 2
-    // const int &ref = get_element(my_vector, 2);
+  // // Get a reference to the element at index 2
+  // const int &ref = get_element(my_vector, 2);
 
-    // // No intention of modifying the returned reference
-    // std::cout << ref << '\n';
+  // // No intention of modifying the returned reference
+  // std::cout << ref << '\n';
 
-    //==============
-    //======== 19
-    //==============
-    // f() = 5;         // 1
-    // auto &ref = f(); // 5
-    // ref = 10;
-    // f(); // 10
+  //======== 19
+  //==============
+  // f() = 5;         // 1
+  // auto &ref = f(); // 5
+  // ref = 10;
+  // f(); // 10
 
-    //==============
-    //======== 20
-    //==============
-    // std::cout << add(2, 3) << '\n';
+  //======== 20
+  //==============
+  // std::cout << add(2, 3) << '\n';
 
-    //==============
-    //======== 21
-    //==============
+  //======== 21
+  //==============
 
-    //==============
-    //======== 22
-    //==============
-    // std::cout << add(2, 3) << '\n';           // add(int, int)
-    // std::cout << add(2, 3.5) << '\n';         // add(int, double)
-    // std::cout << add(2.5, 3) << '\n';         // add(double, int)
-    // std::cout << add(2.5, 3.2) << '\n';       // add(double, double)
-    // std::cout << add(2.5, 3.2, 4.75) << '\n'; // add(double, double, double)
+  //======== 22
+  //==============
+  // std::cout << add(2, 3) << '\n';           // add(int, int)
+  // std::cout << add(2, 3.5) << '\n';         // add(int, double)
+  // std::cout << add(2.5, 3) << '\n';         // add(double, int)
+  // std::cout << add(2.5, 3.2) << '\n';       // add(double, double)
+  // std::cout << add(2.5, 3.2, 4.75) << '\n'; // add(double, double, double)
 
-    //==============
-    //======== 23
-    //==============
+  //======== 23
+  //==============
 
-    //==============
-    //======== 24
-    //==============
+  //======== 24
+  //==============
 
-    // std::cout << add(2, 3) << '\n';    // add(int, int) -- exact match
-    // std::cout << add(2, 3.5f) << '\n'; // add(int, float) -- exact match
-    // //==============
-    // std::cout << add(2.5, 3) << '\n';     // add(double, int) -- ???
-    // std::cout << add('h', false) << '\n'; // add(char, bool) -- ???
+  // std::cout << add(2, 3) << '\n';    // add(int, int) -- exact match
+  // std::cout << add(2, 3.5f) << '\n'; // add(int, float) -- exact match
+  // //==============
+  // std::cout << add(2.5, 3) << '\n';     // add(double, int) -- ???
+  // std::cout << add('h', false) << '\n'; // add(char, bool) -- ???
 
-    //==============
-    //======== 25
-    //==============
+  //======== 25
+  //==============
 
-    //==============
-    //======== 26
-    //==============
+  //======== 26
+  //==============
 
-    //==============
-    //======== 27
-    //==============
-    // int x{10};
-    // int y{20};
-    // int z{};
-    // z = g(x, y);
-    // std::cout << z << '\n';
+  //======== 27
+  //==============
+  // int x{10};
+  // int y{20};
+  // int z{};
+  // z = g(x, y);
+  // std::cout << z << '\n';
 
-    //==============
-    //======== 28
-    //==============
-    // int n{};
-    // std::cout << "Enter a number: ";
-    // std::cin >> n;
-    // std::cout << "Factorial of " << n << " is " << factorial(n) << '\n';
+
+  //======== 28
+  //==============
+  // int n{};
+  // std::cout << "Enter a number: ";
+  // std::cin >> n;
+  // std::cout << "Factorial of " << n << " is " << factorial(n) << '\n';
 }
 
-//==============
+
 //======== 29
 //==============
 // int main(int argc, char *argv[]) {
