21f833ee9b525f67b133637a1d9f3ef8d6ed6d5a
Zeid Kootbally <zeidk@umd.edu>
2025-07-27 22:28:25 -0400

    Completed modern C++ features

--- lecture8/src/optional_demo.cpp
+++ lecture8/src/optional_demo.cpp
@@ -8,19 +8,19 @@
     return a / b;  // Has value
 }
 
-//main.cpp
-// int main() {
-//     std::optional<int> result{divide(10, 3)};
-//     if (result) {  // Clear: check if has value
-//         std::cout << *result << '\n';  // Safe access
-//         std::cout << result.value() << '\n';  // Safe access
-//     }
-// }
-
+main.cpp
 int main() {
-    std::optional<int> opt_with_value{10};
-    std::optional<int> opt_empty{std::nullopt};
-
-    std::cout << "opt_with_value: " << opt_with_value.value_or(-1) << "\n"; // 10
-    std::cout << "opt_empty: " << opt_empty.value_or(-1) << "\n";         // -1
+    std::optional<int> result{divide(10, 3)};
+    if (result) {  // Clear: check if has value
+        std::cout << *result << '\n';  // Safe access
+        std::cout << result.value() << '\n';  // Safe access
+    }
 }
+
+// int main() {
+//     std::optional<int> opt_with_value{10};
+//     std::optional<int> opt_empty{std::nullopt};
+
+//     std::cout << "opt_with_value: " << opt_with_value.value_or(-1) << "\n"; // 10
+//     std::cout << "opt_empty: " << opt_empty.value_or(-1) << "\n";         // -1
+// }
